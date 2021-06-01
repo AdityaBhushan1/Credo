b2772a7b7ea3f51962116835a61dd6d8b544173d
TierGamerpy <adityabhushan934@gmail.com>
2021-06-01 09:19:46 +0530

    Update inputs.py

--- cogs/utils/inputs.py
+++ cogs/utils/inputs.py
@@ -1,21 +1,19 @@
 import asyncio
-import re
 import dateparser
-from datetime import datetime
-from discord.ext.commands.converter import RoleConverter, TextChannelConverter
-import pytz
+from datetime import datetime, timedelta
+from discord.ext.commands.converter import RoleConverter, TextChannelConverter, MemberConverter
+from . import exceptions,emote
 import discord
-from . import emote
-
+import pytz
 IST = pytz.timezone("Asia/Kolkata")
-class InputError:
-    ...
 
 async def safe_delete(message):
     try:
         await message.delete()
     except (discord.Forbidden, discord.NotFound):
-        return
+        return False
+    else:
+        return True
 
 
 async def channel_input(ctx, check, timeout=120, delete_after=False):
@@ -23,21 +21,11 @@ async def channel_input(ctx, check, timeout=120, delete_after=False):
         message = await ctx.bot.wait_for("message", check=check, timeout=timeout)
         channel = await TextChannelConverter().convert(ctx, message.content)
     except asyncio.TimeoutError:
-        return await ctx.send(f"{emote.error} | You failed to select a channel in time. Try again!")
+        raise exceptions.InputError(f"{emote.error} | You failed to select a channel in time. Try again!")
 
     else:
-        if len(message.channel_mentions) == 0:
-            return await message.channel.send(f'{emote.error} | Thats Not A Channel')
         if not channel.permissions_for(ctx.me).read_messages:
-            return await ctx.send(
-                f"{emote.error} | Unfortunately, I don't have read messages permissions in {channel.mention}."
-            )
-            
-        if not channel.permissions_for(ctx.me).send_messages:
-            return await ctx.send(
-                f"{emote.error} | Unfortunately, I don't have send messages permissions in {channel.mention}."
-            )    
-
+            raise exceptions.InputError(f"{emote.error} | I don't have send messages permissions in {channel.mention}.")
 
         if delete_after:
             await safe_delete(message)
@@ -50,23 +38,24 @@ async def role_input(ctx, check, timeout=120, hierarchy=True, delete_after=False
         message = await ctx.bot.wait_for("message", check=check, timeout=timeout)
         role = await RoleConverter().convert(ctx, message.content)
     except asyncio.TimeoutError:
-        raise InputError(f"{emote.error} | You failed to select a role in time. Try again!")
+        raise exceptions.InputError(f"{emote.error} | You failed to select a role in time. Try again!")
 
     else:
-        if len(message.role_mentions) == 0:
-            return await ctx.send(f'{emote.error} | Thats Not A Role')
-
         if hierarchy is True:
             if role > ctx.me.top_role:
-                return await ctx.send(
+                raise exceptions.InputError(
                     f"{emote.error} | The position of {role.mention} is above my top role. So I can't give it to anyone.\nKindly move {ctx.me.top_role.mention} above {role.mention} in Server Settings."
                 )
 
             if ctx.author.id != ctx.guild.owner_id:
                 if role > ctx.author.top_role:
-                    return await ctx.send(
+                    raise exceptions.InputError(
                         f"{emote.error} | The position of {role.mention} is above your top role {ctx.author.top_role.mention}."
                     )
+            
+            if role.managed:
+                raise exceptions.InputError(f"{emote.error} | Role is an integrated role and cannot be added manually.")
+
 
         if delete_after:
             await safe_delete(message)
@@ -74,18 +63,29 @@ async def role_input(ctx, check, timeout=120, hierarchy=True, delete_after=False
         return role
 
 
-async def integer_input(
-    ctx, check, timeout=120, limits=(None, None), delete_after=False
-):
+async def member_input(ctx, check, timeout=120, delete_after=False):
+    try:
+        message = await ctx.bot.wait_for("message", check=check, timeout=timeout)
+        member = await MemberConverter().convert(ctx, message.content)
+
+    except asyncio.TimeoutError:
+        raise exceptions.InputError(f"{emote.error} | You failed to mention a member in time. Try again!")
+
+    else:
+        if delete_after:
+            await safe_delete(message)
+
+        return member
+
+
+async def integer_input(ctx, check, timeout=120, limits=(None, None), delete_after=False):
     def new_check(message):
         if not check(message):
             return False
 
         try:
             if limits[1] is not None:
-                if len(message.content) > len(
-                    str(limits[1])
-                ):  # This is for safe side, memory errors u know :)
+                if len(message.content) > len(str(limits[1])):  # This is for safe side, memory errors u know :)
                     return False
 
             digit = int(message.content)
@@ -109,7 +109,7 @@ async def integer_input(
     try:
         message = await ctx.bot.wait_for("message", check=new_check, timeout=timeout)
     except asyncio.TimeoutError:
-        raise InputError(f"{emote.error} | You failed to select a number in time. Try again!")
+        raise exceptions.InputError(f"{emote.error} | You failed to select a number in time. Try again!")
     else:
         if delete_after:
             await safe_delete(message)
@@ -121,49 +121,35 @@ async def time_input(ctx, check, timeout=120, delete_after=False):
     try:
         message = await ctx.bot.wait_for("message", check=check, timeout=timeout)
     except asyncio.TimeoutError:
-        return await ctx.send(f"{emote.error} | Timeout, You have't responsed in time. Try again!")
+        raise exceptions.InputError(f"{emote.error} | Timeout, You have't responsed in time. Try again!")
     else:
-        match = re.match(r"\d+:\d+", message.content)
-        if not match:
-            return await ctx.send(f'{emote.error} | Thats Not A Valid Time')
-        match = match.group(0) 
-        hour, minute = match.split(":")
-        str_time = f'{hour}:{minute}'
-        converting = datetime.strptime(str_time,'%H:%M')
-        final_time = converting.time()
+        try:
+            parsed = dateparser.parse(
+                message.content,
+                settings={
+                    "RELATIVE_BASE": datetime.now(tz=IST),
+                    "TIMEZONE": "Asia/Kolkata",
+                    "RETURN_AS_TIMEZONE_AWARE": True,
+                },
+            )
 
-        return final_time
+            if delete_after:
+                await safe_delete(message)
 
+            if datetime.now(tz=IST) > parsed:
+                parsed = parsed + timedelta(hours=24)
 
-# async def time_input(ctx, check, timeout=120, delete_after=False):
-#     try:
-#         message = await ctx.bot.wait_for("message", check=check, timeout=timeout)
-#     except asyncio.TimeoutError:
-#         raise InputError("Timeout, You have't responsed in time. Try again!")
-#     else:
-#         try:
-#             parsed = dateparser.parse(
-#                 message.content,
-#                 settings={
-#                     "RELATIVE_BASE": datetime.now(tz=IST),
-#                     "TIMEZONE": "Asia/Kolkata",
-#                     "RETURN_AS_TIMEZONE_AWARE": True,
-#                 },
-#             )
+            return parsed
 
-#             if delete_after:
-#                 await safe_delete(message)
-
-#             return parsed
-#         except TypeError:
-#             raise InputError("This isn't valid time format.")
+        except TypeError:
+            raise exceptions.InputError(f"{emote.error} | This isn't valid time format.")
 
 
 async def string_input(ctx, check, timeout=120, delete_after=False):
     try:
         message = await ctx.bot.wait_for("message", check=check, timeout=timeout)
     except asyncio.TimeoutError:
-        raise InputError("Response timed out!")
+        raise exceptions.InputError(f"{emote.error} | Took too long. Good Bye.")  # This would sound cooler.
     else:
         if delete_after:
             await safe_delete(message)
