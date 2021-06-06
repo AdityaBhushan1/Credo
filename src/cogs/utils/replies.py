a9d2133baf199954b9330563bbe0dd14dab7594b
TierGamerpy <adityabhushan934@gmail.com>
2021-06-06 11:34:57 +0530

    Create replies.py

---
+++ src/cogs/utils/replies.py
@@ -0,0 +1,59 @@
+ERROR_REPLIES = [
+    "Please don't do that.",
+    "You have to stop.",
+    "Do you mind?",
+    "In the future, don't do that.",
+    "That was a mistake.",
+    "You blew it.",
+    "You're bad at computers.",
+    "Are you trying to kill me?",
+    "Noooooo!!",
+    "I can't believe you've done this",
+]
+
+NEGATIVE_REPLIES = [
+    "Noooooo!!",
+    "Nope.",
+    "I don't think so.",
+    "Not gonna happen.",
+    "Huh? No.",
+    "Nah.",
+    "Naw.",
+    "Not likely.",
+    "No way,",
+    "Not in a million years.",
+    "Fat chance.",
+    "Certainly not.",
+    "NEGATORY.",
+    "Nuh-uh.",
+    "Not in my house!",
+]
+
+POSITIVE_REPLIES = [
+    "Yep.",
+    "Absolutely!",
+    "Can do!",
+    "Affirmative!",
+    "Yeah okay.",
+    "Sure.",
+    "Sure thing!",
+    "You're the boss!",
+    "Okay.",
+    "No problem.",
+    "I got you.",
+    "Alright.",
+    "You got it!",
+    "ROGER THAT",
+    "Of course!",
+    "Aye aye, cap'n!",
+    "I'll allow it.",
+]
+OWNER_MENTION_REPLIES=[
+    ':expressionless:',
+    '<:emoji_34:806415181332742194>',
+    '<:emoji_35:806415225083265026>',
+    '<:emoji_30:806415048506998814>',
+    '<:emoji_33:806415151585820692>',
+    '<:emoji_31:806415082798972928>',
+    '<:emoji_29:806414962746327081>'
+]
